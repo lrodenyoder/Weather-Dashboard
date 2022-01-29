c8d00018be6b7dfd799beed3ec84b06328384c5d
lrodenyoder <rodenlau000@gmail.com>
2022-01-29 11:38:20 -0600

    beginning to mess with localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // VARIABLES
 var cityHistoryArray = [];
+console.log(cityHistoryArray);
 var cityHistoryCounter = 0;
+//containers
 var cityFormEl = document.getElementById("city-form");
 var cityNameInputEl = document.getElementById("city");
 var currentWeatherEl = document.getElementById("current-weather-list-container");
@@ -9,6 +11,7 @@ var displayCurrentCity = document.getElementById("current-city");
 var iconEl = document.getElementById("icon");
 var forecastWrapperEl = document.getElementById("five-day-card-wrapper");
 var historyContainerEl = document.getElementById("history-container");
+
 //display dates
 var currentDate = moment().format("L");
 var displayCurrentDate = document.getElementById("current-date");
@@ -66,128 +69,146 @@ var getWeatherData = function (cityLat, cityLon) {
 };
 
 var displayWeather = function (weather) {
-    //set weather icon
-    var iconImg = weather.current.weather[0].icon;
-    icon.setAttribute(
-        "src",
-        "http://openweathermap.org/img/wn/" + iconImg + ".png"
-    );
+    //debugger;
+    console.log(currentWeatherEl.textContent);
+    if (currentWeatherEl.textContent == "") {
+        //set weather icon
+        var iconImg = weather.current.weather[0].icon;
+        icon.setAttribute(
+            "src",
+            "http://openweathermap.org/img/wn/" + iconImg + ".png"
+        );
 
-    weatherIcon.appendChild(icon);
+        weatherIcon.appendChild(icon);
 
-    //create ul container
-    var weatherList = document.createElement("ul");
-    weatherList.classList = "curr-weather-list list-unstyled";
-    weatherList.id = "curr-weather-list";
+        //create ul container
+        var weatherList = document.createElement("ul");
+        weatherList.classList = "curr-weather-list list-unstyled";
+        weatherList.id = "curr-weather-list";
 
-    //set current temp
-    var tempLi = document.createElement("li");
-    tempLi.classList = "curr-weather-list-item";
-    tempLi.id = "curr-temp";
-    tempLi.textContent = "Temperature: " + weather.current.temp + "°F";
+        //set current temp
+        var tempLi = document.createElement("li");
+        tempLi.classList = "curr-weather-list-item";
+        tempLi.id = "curr-temp";
+        tempLi.textContent = "Temperature: " + weather.current.temp + "°F";
 
-    weatherList.appendChild(tempLi);
+        weatherList.appendChild(tempLi);
 
-    //set current wind speed
-    var windSpeedLi = document.createElement("li");
-    windSpeedLi.classList = "curr-weather-list-item";
-    windSpeedLi.id = "curr-wind";
-    windSpeedLi.textContent = "Wind Speed: " + weather.current.wind_speed + " MPH";
+        //set current wind speed
+        var windSpeedLi = document.createElement("li");
+        windSpeedLi.classList = "curr-weather-list-item";
+        windSpeedLi.id = "curr-wind";
+        windSpeedLi.textContent = "Wind Speed: " + weather.current.wind_speed + " MPH";
 
-    weatherList.appendChild(windSpeedLi);
+        weatherList.appendChild(windSpeedLi);
 
-    //set current humidity
-    var humidLi = document.createElement("li");
-    humidLi.classList = "curr-weather-list-item";
-    humidLi.id = "curr-humid";
-    humidLi.textContent = "Humidity: " + weather.current.humidity + "%";
+        //set current humidity
+        var humidLi = document.createElement("li");
+        humidLi.classList = "curr-weather-list-item";
+        humidLi.id = "curr-humid";
+        humidLi.textContent = "Humidity: " + weather.current.humidity + "%";
 
-    weatherList.appendChild(humidLi);
+        weatherList.appendChild(humidLi);
 
-    //set current uvi
-    var uviLi = document.createElement("li");
-    uviLi.id = "curr-uvi";
-    uviLi.innerHTML = "UV Index:  <span id='uvi-color'></span>";
+        //set current uvi
+        var uviLi = document.createElement("li");
+        uviLi.id = "curr-uvi";
+        uviLi.innerHTML = "UV Index:  <span id='uvi-color'></span>";
    
-    weatherList.appendChild(uviLi);
+        weatherList.appendChild(uviLi);
 
-    //append ul to html container
-    currentWeatherEl.appendChild(weatherList);
+        //append ul to html container
+        currentWeatherEl.appendChild(weatherList);
 
-    //add uvi text and class after appending list to allow for variable bg color
-    var uviText = weather.current.uvi;
-    var uviLiSpan = document.getElementById("uvi-color");
-    uviLiSpan.textContent = uviText;
+        //add uvi text and class after appending list to allow for variable bg color
+        var uviText = weather.current.uvi;
+        var uviLiSpan = document.getElementById("uvi-color");
+        uviLiSpan.textContent = uviText;
 
-    //change uvi bg color based on uv index value
-    if (uviText >= 8 && uviText <=10) { 
-        uviLiSpan.classList = "curr-weather-list-item text-white bg-danger";
-    }else if (uviText >= 6 && uviText <=7) { 
-        uviLiSpan.classList = "curr-weather-list-item text-white bg-warning";
-    }else if (uviText >= 3 && uviText <=5) {
-        uviLiSpan.classList = "curr-weather-list-item";
-        uviLiSpan.setAttribute("style", "background-color: yellow");
-    } else if (uviText >= 0 && uviText <=2) {
-        uviLiSpan.classList = "curr-weather-list-item text-white bg-success";
+        //change uvi bg color based on uv index value
+        if (uviText >= 8 && uviText <= 10) {
+            uviLiSpan.classList = "curr-weather-list-item text-white bg-danger";
+        } else if (uviText >= 6 && uviText <= 7.99) {
+            uviLiSpan.classList = "curr-weather-list-item text-white bg-warning";
+        } else if (uviText >= 3 && uviText <= 5.99) {
+            uviLiSpan.classList = "curr-weather-list-item";
+            uviLiSpan.setAttribute("style", "background-color: yellow");
+        } else if (uviText >= 0 && uviText <= 2.99) {
+            uviLiSpan.classList = "curr-weather-list-item text-white bg-success";
+        } else {
+            uviLiSpan.textContent = "Error: Value Out Of Bounds"
+        }
     } else {
-        uviLiSpan.textContent = "Error: Value Out Of Bounds"
+        return;
     }
 };
 
 var displayForecast = function (weather) {
-    //loop through next 5 days to create forecast cards
-    for (var i = 0; i < 5; i++) {
-        // create card
-        var displayForecastCard = document.createElement("div");
-        displayForecastCard.id = "day-" + i + "-card";
-        displayForecastCard.classList = "text-center col-sm col-md col-lg five-day-card";
+    if (forecastWrapperEl.textContent == "") {
+        //loop through next 5 days to create forecast cards
+        for (var i = 0; i < 5; i++) {
+            // create card
+            var displayForecastCard = document.createElement("div");
+            displayForecastCard.id = "day-" + i + "-card";
+            displayForecastCard.classList =
+                "text-center col-sm col-md col-lg five-day-card";
 
-        //create h3 with date
-        var displayForecastDateEl = document.createElement("h3");
-        displayForecastDateEl.id = "card-" + i;
-        displayForecastDateEl.textContent = moment().add(i + 1, "day").format("L");
+            //create h3 with date
+            var displayForecastDateEl = document.createElement("h3");
+            displayForecastDateEl.id = "card-" + i;
+            displayForecastDateEl.textContent = moment()
+                .add(i + 1, "day")
+                .format("L");
 
-        //create list of weather info
-        var displayForecastList = document.createElement("ul");
-        displayForecastList.classList = "future-weather-list list-unstyled";
+            //create list of weather info
+            var displayForecastList = document.createElement("ul");
+            displayForecastList.classList = "future-weather-list list-unstyled";
 
-        //create icon list item
-        var displayForecastIcon = document.createElement("li");
-        var imgEl = document.createElement("img");
-        var iconImg = weather.daily[i].weather[0].icon;
-        imgEl.setAttribute(
-          "src",
-          "http://openweathermap.org/img/wn/" + iconImg + ".png"
-        );
-        displayForecastIcon.appendChild(imgEl);
-        displayForecastList.appendChild(displayForecastIcon);
+            //create icon list item
+            var displayForecastIcon = document.createElement("li");
+            var imgEl = document.createElement("img");
+            var iconImg = weather.daily[i].weather[0].icon;
+            imgEl.setAttribute(
+                "src",
+                "http://openweathermap.org/img/wn/" + iconImg + ".png"
+            );
+            displayForecastIcon.appendChild(imgEl);
+            displayForecastList.appendChild(displayForecastIcon);
 
-        //create temp list item
-        var displayForecastTemp = document.createElement("li");
-        displayForecastTemp.classList = "forecast-list-item";
-        displayForecastTemp.textContent = "Temp: " + weather.daily[i].temp.day + "°F";
-        displayForecastList.appendChild(displayForecastTemp);
+            //create temp list item
+            var displayForecastTemp = document.createElement("li");
+            displayForecastTemp.classList = "forecast-list-item";
+            displayForecastTemp.textContent =
+                "Temp: " + weather.daily[i].temp.day + "°F";
+            displayForecastList.appendChild(displayForecastTemp);
 
-        //create wind speed list item
-        var displayForecastWind = document.createElement("li");
-        displayForecastWind.classList = "forecast-list-item";
-        displayForecastWind.textContent = "Wind: " + weather.daily[i].wind_speed + " MPH";
-        displayForecastList.appendChild(displayForecastWind);
+            //create wind speed list item
+            var displayForecastWind = document.createElement("li");
+            displayForecastWind.classList = "forecast-list-item";
+            displayForecastWind.textContent =
+                "Wind: " + weather.daily[i].wind_speed + " MPH";
+            displayForecastList.appendChild(displayForecastWind);
 
-        //create humidity list item
-        var displayForecastHumid = document.createElement("li");
-        displayForecastHumid.classList = "forecast-list-item";
-        displayForecastHumid.textContent = "Humidity: " + weather.daily[i].humidity + "%";
-        displayForecastList.appendChild(displayForecastHumid);
+            //create humidity list item
+            var displayForecastHumid = document.createElement("li");
+            displayForecastHumid.classList = "forecast-list-item";
+            displayForecastHumid.textContent =
+                "Humidity: " + weather.daily[i].humidity + "%";
+            displayForecastList.appendChild(displayForecastHumid);
 
-
-        displayForecastCard.appendChild(displayForecastDateEl);
-        displayForecastCard.appendChild(displayForecastList);
-        forecastWrapperEl.appendChild(displayForecastCard);
+            displayForecastCard.appendChild(displayForecastDateEl);
+            displayForecastCard.appendChild(displayForecastList);
+            forecastWrapperEl.appendChild(displayForecastCard);
+        }
+    } else {
+        return;
     }
+    //debugger;
+    
 };
 
 var saveCityHistory = function () {
+  
     localStorage.setItem("cityHistory", JSON.stringify(cityHistoryArray));
     createHistoryList(cityHistoryArray);
 };
@@ -210,19 +231,17 @@ var createHistoryList = function (cityHistory) {
     
     var historyCityDiv = document.createElement("div");
     historyCityDiv.classList = "row";
-    //debugger;
-    console.log(historyContainerEl);
     
     //loops through array to create history buttons
     for (var i = 0; i < sixCities.length; i++) {
       var historyCity = document.createElement("button");
-      historyCity.classList = "city-btn col-4 col-md-4 col-lg-12";
+        historyCity.classList = "city-btn col-4 col-md-4 col-lg-12";
+        historyCity.id = "city-btn";
       historyCity.setAttribute("data-city-id", cityHistoryCounter);
       historyCity.textContent = (sixCities[i]);
       
       historyCityDiv.appendChild(historyCity);
     }
-
     historyContainerEl.appendChild(historyCityDiv);
 };
 
@@ -246,7 +265,6 @@ var formSubmitHandler = function (event) {
         displayCurrentDate.textContent = "(" + currentDate + ")";
         getCityCoord(city);
         saveCityHistory();
-        // loadCityHistory(); ??
     } else {
         alert("Please enter a valid city")
     }
@@ -254,5 +272,37 @@ var formSubmitHandler = function (event) {
 
 //EVENT LISTENERS
 cityFormEl.addEventListener("submit", formSubmitHandler);
+document.addEventListener("click", function (event) {
+    if (event.target && event.target.id == "city-btn") {
+        var city = event.target.textContent;
+        console.log(city);
+        getCityCoord(city);
+        currentWeatherEl.textContent = "";
+        forecastWrapperEl.textContent = "";
+        historyContainerEl.textContent = "";
+        iconEl.removeAttribute("src");
+        cityHistoryArray.unshift(city);
+
+        if (city) {
+          cityNameInputEl.value = "";
+          displayCurrentCity.textContent = city;
+          displayCurrentDate.textContent = "(" + currentDate + ")";
+          getCityCoord(city);
+          saveCityHistory();
+        } else {
+          alert("Please enter a valid city");
+        }
+    } else {
+        return;
+    }
+});
+
+var cityHistoryHandler = function (event) {
+    var city = event.target.getAttribute("city-btn");
+    console.log(city);
+}
+
 
 loadCityHistory();
+
+//TO DO: GET LOCAL TIME FROM LOCAL TIME ZONES. EG: TOKYO IS 9 HOURS AHEAD, DATES CAN BE DIFFERENT
